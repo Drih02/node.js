a3225c90308c20c0413b44214e75cda4aa91f008
adrielle <p02ms@outlook.com>
2024-03-14 09:59:40 -0300

    parte 2

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import http from "http";
 
 const PORT= 3000;
+const rotas = {
+    "/":"Bem Vindo a Rota Inicial",
+    "/livros":"Rota Livro",
+    "/autores":"Rota Autores"
+}
 
 const server = http.createServer((req, res)  => {
     res.writeHead(200,{'Content-type':'text/plan'});
-    res.end('Hello Word!');
+    res.end(rotas[req.url]);
 });
 
     server.listen(PORT,() => {
